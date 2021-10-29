ba0ea94303cdda167cf5ab9fd475a71610c21e91
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2021-10-29 11:04:07 +0100

    alt1

--- trabalhopgm/src/trabalhopgm/Trabalhopgm.java
+++ trabalhopgm/src/trabalhopgm/Trabalhopgm.java
@@ -4,5 +4,6 @@ public class Trabalhopgm {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("alterações feitas por outro user");
+        System.out.println("faço alterarões");
     }  
 }

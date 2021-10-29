402464ab36620a6228362573eff3a34256f70a98
fishingtest <guilherme30454@esfundao.pt>
2021-10-29 10:55:57 +0100

    adicionadas por outro user

--- trabalhopgm/src/trabalhopgm/Trabalhopgm.java
+++ trabalhopgm/src/trabalhopgm/Trabalhopgm.java
@@ -3,5 +3,6 @@ package trabalhopgm;
 public class Trabalhopgm {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("alterações feitas por outro user");
     }  
 }
